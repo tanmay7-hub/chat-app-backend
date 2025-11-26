283e7ea91b67b83a035bd502fdbc0170756b6402
tanmay joshi <tanmayjoshi411@gmail.com>
2025-11-27 00:27:57 +0530

    Update CORS origin to production URL

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-frontend-sable-xi.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
