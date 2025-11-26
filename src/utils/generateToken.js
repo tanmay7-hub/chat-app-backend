5bf61dc3795d7666bfbe82a7f5c453b31f9e453b
tanmay joshi <tanmayjoshi411@gmail.com>
2025-11-27 00:50:05 +0530

    Change cookie secure flag to always true

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateToken=(userId,res)=>{
     );
     res.cookie("token",token,{
         httpOnly:true,
-        secure:process.env.NODE_ENV==="production",
+        secure:"true",
         sameSite:"none",
         maxAge:7*24*60*60*1000 // 7 days
     })
